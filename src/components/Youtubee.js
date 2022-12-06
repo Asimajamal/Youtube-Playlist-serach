42c0605afd106942d7fc3594d8f4fc871f42d746
asima jamal <asima jama>
2022-12-06 20:21:43 +0500

    First Commit

--- src/components/Youtubee.js
+++ src/components/Youtubee.js
@@ -1,56 +1,223 @@
-import React ,{ useEffect, useState } from "react";
+//code for a unique playlist and searching any video in that playlist
+// import React, { useEffect, useState } from "react";
 
+// const Youtubee = () => {
+//   const [videos, setVideos] = useState([]);
+//   const [search, setSearch] = useState("");
+//   const [filter, setFilter] = useState([]);
 
+//   const fetchData = async () => {
+//     const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLxxubppkxDkdjC1JJivQyiAGpMNTtgSxo&maxResults=50&key=AIzaSyB7MoEqFcw2eWTnf63op_eOnQhGueluASY`;
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     setVideos(data.items);
+//     setFilter(data.items);
+//   };
 
-const Youtubee=()=>
+//   const handelChange = (e) => {
+//     setSearch(e.target.value);
+//   };
 
-{
-    const[videos,setVideos]=useState([]);
-    const[search,setSearch]=useState('')
+//   const handelSubmit = (e) => {
+//     e.preventDefault();
 
-    useEffect(()=>{
+//     const newFilter = videos.filter((video) => {
+//       //  video.snippet.title
+//       //   .toLocaleLowerCase()
+//       //   .includes(search.toLocaleLowerCase());
 
-        const fetchApi= ()=>{
-            const url=`https://www.googleapis.com/youtube/v3/playlistItems?q=${search}&appid=AIzaSyB7MoEqFcw2eWTnf63op_eOnQhGueluASY`
-            fetch(url).then((response)=>response.json()).then((resJson)=>{
-                const result=resJson.items.map(playlistt=>({
-                    ...playlistt
-                }));
+//       //console.log(`VIDEO TITLE: ${video.snippet.title} == ${search}`);
+
+//       if (
+//         video.snippet.title
+//           .toLocaleLowerCase()
+//           .includes(search.toLocaleLowerCase())
+//       ) {
+//         console.log("Displaying video")
+//         //console.log(video);
+//         return (     
+//                       <div>{video}</div>
+//                 );
+//       }
+      
+      
+//     });
+
+//     setFilter(newFilter);
+//   };
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className={{ width: "100vw", height: "100vh" }}>
+//       <form
+//         onSubmit={handelSubmit}
+//         style={{ width: "70%", marginLeft: "auto", marginRight: "auto" }}
+//       >
+//         <input
+//           type="search"
+//           placeholder="Enter Url"
+//           onChange={(e) => handelChange(e)}
+//           value={search}
+//           style={{
+//             margin: "2rem",
+//             padding: "1rem",
+//             fontSize: "1.4rem",
+//             width: "60%",
+//             marginLeft: "auto",
+//             marginRight: "auto",
+//           }}
+//         />
+
+//         <button
+//           type="submit"
+//           style={{
+//             padding: "1rem",
+//             fontSize: "1.4rem",
+//           }}
+//         >
+//           Search
+//         </button>
+//       </form>
+
+//       {filter.map((video, key) => {
+//         return (
+//           <div key={key} style={{ backgroundColor: "#fcf4bd" }}>
             
-             
-                setVideos(result.contentDetails);
-            })
+//               <div style={{ margin: "1rem", width: "100%", display: "flex" }}>
+//                 <div style={{ width: "40%" }}>
+//                   <iframe
+//                     width={450}
+//                     height={200}
+//                     style={{ margin: "1rem" }}
+//                     src={`https:www.youtube.com/embed/${video.snippet.resourceId.videoId} `}
+//                     title="YouTube video player"
+//                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//                     allowFullScreen
+//                   ></iframe>
+//                 </div>
+
+//                 <div style={{ margin: "1rem", width: "60%" }}>
+//                   <b>{video.snippet.title}</b>
+//                   <p>{video.snippet.description.split(".")[0]}</p>
+//                 </div>
+//               </div>
+//             )
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default Youtubee;
+
+//code for searching any Playlist thorugh URL  and fetching all videos on page in that playlist
+import React, {  useState } from "react";
+import VideoPlay from "./VideoPlay";
+// import VideoPlay from "./VideoPlay";
+
+const Youtubee = () => {
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState([]);
+
+  const fetchData = async () => {
+    const playlistUrl = search.split("=");
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${
+      playlistUrl[playlistUrl.length - 1]
+    }&maxResults=50&key=AIzaSyB7MoEqFcw2eWTnf63op_eOnQhGueluASY`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setFilter(data.items);
+  };
+
+  const handelChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  const handleClick=(value)=>
+  {
+      window.open(value,'_blank')
+  }
+  
+
+  return (
+    <div className={{ width: "100vw", height: "100vh" }}>
+      <form
+        onSubmit={handelSubmit}
+        style={{ width: "70%",  marginLeft: "auto", marginRight: "auto" }}
+      >
+        <input
+          type="search"
+          placeholder="Enter Url"
+          onChange={(e) => handelChange(e)}
+          value={search}
+          style={{
+            margin: "2rem",
+            padding: "1rem",
+            fontSize: "1.4rem",
+            width: "85%",
+            marginLeft: "auto",
+            marginRight: "auto",
             
+          }}
+        />
 
-        }
-        fetchApi();
-    },[search])
+        <button
+          type="submit"
+          style={{
+            padding: "1rem",
+            fontSize: "1.4rem",
+           
+          }}
+        >
+          Search
+        </button>
+      </form>
 
-    return(
+      {filter.length > 0 ? (
+        filter.map((video) => {
+          return (
+            <div key={video.id} style={{ backgroundColor: "#fcf6bd" }}>
+              
+               
+                <div style={{ margin: "1rem", width: "100%", display: "flex" }}>
+                  <div style={{ width: "40%" }}>
+                   
+                      <iframe  width={450}
+                    
+                      height={200}
+                      style={{ margin: "1rem" }}
+                      src={`https:www.youtube.com/embed/${video.snippet.resourceId.videoId}  ` }
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay 'src'; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      onClick={()=>handleClick(`https:www.youtube.com/embed/${video.snippet.resourceId.videoId}  `)}
+                    ></iframe>
+                    
+                  </div>
+                  
 
-    <div>
-
-        {
-            videos.map((item)=>{
-                return(
-                    <div>
-                        <div>
-                        <input type="search" placeholder="Enter Url" onChange={(e)=>{
-                     setSearch(e.target.value)
-                    }}></input></div>
-                    {
-                    !item? (<h1>No Url Found</h1>)
-                    :(<div>{item.videoId}</div>)
-                    });
-
-
-
-                    </div>
-                );
-            })
-        }
-        
-    </div> 
-)}
+                  <div style={{ margin: "1rem", width: "60%" }}>
+                    <b>{video.snippet.title}</b>
+                    <p>{video.snippet.description.split(".")[0]}</p>
+                  </div>
+                </div>
+              
+            </div>
+          );
+        })
+      ) : (
+        <h1 style={{ marginLeft: "30%" }}>Playlist Videos will be display here</h1>
+      )}
+    </div>
+  );
+};
 
 export default Youtubee;
